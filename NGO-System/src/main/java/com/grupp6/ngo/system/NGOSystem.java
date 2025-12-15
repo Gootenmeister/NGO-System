dc9b8ccb50462774d4b91af5399f55a4a6e1a183
Gooten <Gootenmeister@users.noreply.github.com>
2025-12-15 13:41:48 +0100

    Init
    asasdsa

---
+++ NGO-System/src/main/java/com/grupp6/ngo/system/NGOSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupp6.ngo.system;
+
+/**
+ *
+ * @author gustav
+ */
+public class NGOSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Häst123!");
+    }
+}
